e453da561cef8641b611d02ebd8a9c31656a0814
jminowitz <joe.minowitz@hautelook.com>
2018-02-26 10:15:38 -0800

    Added all

---
+++ recipes/default.rb
@@ -0,0 +1,6 @@
+#
+# Cookbook:: uninstall_puppet
+# Recipe:: default
+#
+# Copyright:: 2018, The Authors, All Rights Reserved.
+ include recipe 'uninstall_puppet::rm_puppet'
---
+++ recipes/rm_puppet.rb
@@ -0,0 +1,14 @@
+#
+# Cookbook:: uninstall_puppet
+# Recipe:: rm_puppet
+#
+# Copyright:: 2018, The Authors, All Rights Reserved.
+
+# Uninstall Puppet from Windows
+powershell_script 'uninstall_puppet' do
+code <<-EOH
+$app = Get-WmiObject -Class Win32_Product | Where-Object { $_.Name -match "Puppet" }
+$app.Uninstall()
+Catch{$_.Exception.Message} 
+EOH
+end
